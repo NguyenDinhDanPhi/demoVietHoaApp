8f3a1bf74bf0324395ad1a1465d6ea514c96a305
danphi <phittnh@gmail.com>
2023-02-23 11:14:46 +0700

    Initial Commit

---
+++ demoVietHoa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoVietHoa
+//
+//  Created by dan phi on 23/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
