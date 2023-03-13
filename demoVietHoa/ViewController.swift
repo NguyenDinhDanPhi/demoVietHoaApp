69637fb229c644a3b7c34aadebbcedbf963bbfa4
danphi <phittnh@gmail.com>
2023-03-13 13:58:02 +0700

    vietHoa

--- demoVietHoa/ViewController.swift
+++ demoVietHoa/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var myLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello".localized()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 32)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(myLabel)
+        myLabel.frame = view.bounds
     }
 
 
 }
 
+extension String {
+    func localized() -> String {
+        return NSLocalizedString(self,
+                                 tableName: "Localizable",
+                                 bundle: .main,
+                                 value: self,
+                                 comment: self)
+    }
+}
